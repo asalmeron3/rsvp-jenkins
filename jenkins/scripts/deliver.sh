6562c31aa27c50246873e730e43902cc40073808
Arturo Salmeron <asalmeron4@dhcp-10-101-246-34.wireless.northwestern.private>
2019-07-25 13:22:19 -0500

    set +x added to deliver.sh file

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -8,16 +8,4 @@ set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
 
-echo 'The following complex command extracts the value of the <name/> element'
-echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
-set -x
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
-set +x
-
-echo 'The following complex command behaves similarly to the previous one but'
-echo 'extracts the value of the <version/> element within <project/> instead.'
-set -x
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-set +x
-
 set +x
